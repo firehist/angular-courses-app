2ba3a09ccd04a4e3d0f9f0e3e79d11f7e5b6a2ae
Benjamin Longearet <firehist@gmail.com>
2017-06-19 11:09:06 +0200

    Fix Observable.throw

--- src/app/shared/models/product.service.ts
+++ src/app/shared/models/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ProductService {
@@ -14,7 +15,14 @@ export class ProductService {
     return this._httpService.get('http://localhost:3000/products')
         .do((res: Response) => console.log(`GET query to '${res.url}': ${res.status}`))
         .map(res => res.json())
-        .catch(error => Observable.throw('Catch an error!', error));
+        .catch(this._handleError('Product was not found'));
+  }
+
+  private _handleError(errorMessage: string) {
+    return (error: any): Observable<Error> => {
+      console.error(errorMessage, error);
+      return Observable.throw(new Error(error));
+    };
   }
 
 }
