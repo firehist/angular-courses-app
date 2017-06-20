b307b862fe398ac3f894bf0fb7bbce7c9ee8fc5c
Benjamin Longearet <firehist@gmail.com>
2017-06-20 18:11:22 +0200

    Add comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/layout/header/header.component';
 
 @NgModule({
+  // Here declare all components, directives or pipes
   declarations: [
     AppComponent,
     HeaderComponent
   ],
+  // Here import external module (@angular/x or others)
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  // Here provide all your service. At the module level it will
+  // provide singleton for all our application
   providers: [],
+  // Here it's used only in the first module (here we got only one AppModule)
+  // to set-up the first component to use
   bootstrap: [AppComponent]
 })
 export class AppModule { }
